32f528b2b47509a3d873d0b97894986c8c347f9d
Denotess <tian.istenic34@gmail.com>
2025-10-05 15:39:39 +0200

    minimal deps for Render

--- app.py
+++ app.py
@@ -476,6 +476,7 @@ def googleCallback():
         return jsonify({"error": "OAuth authentication failed"}), 500
 
 if __name__ == '__main__':
+    from os import getenv
     initDb()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
